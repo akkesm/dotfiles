e762dc391a82ea0eabc5113a54807f730fe165d7
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2021-12-19 12:22:01 +0100

    vim-plugins: autogenerate version

--- overlays/vim-plugins/default.nix
+++ overlays/vim-plugins/default.nix
@@ -1,90 +1,73 @@
 { inputs, lib, vimUtils }:
 
+let
+  genDateVersion = input:
+    lib.concatStringsSep "-" [
+      (builtins.substring 0 4 input.lastModifiedDate)
+      (builtins.substring 4 2 input.lastModifiedDate)
+      (builtins.substring 7 2 input.lastModifiedDate)
+    ];
+in
 {
-  # COQ plugins are updated every day, autogenerate the version
   coq_nvim = vimUtils.buildVimPluginFrom2Nix {
     pname = "coq_nvim";
-    version = with lib; builtins.head (
-      splitString " " (
-        last (
-          splitString
-            "\n"
-            (fileContents "${inputs.coq_nvim}/.github/.agp")
-        )
-      )
-    );
+    version = genDateVersion inputs.coq_nvim;
     src = inputs.coq_nvim;
     meta.homepage = inputs.coq_nvim.url;
   };
 
   coq_artifacts = vimUtils.buildVimPluginFrom2Nix {
     pname = "coq_artifacts";
-    version = with lib; builtins.head (
-      splitString " " (
-        last (
-          splitString
-            "\n"
-            (fileContents "${inputs.coq_artifacts}/.github/.agp")
-        )
-      )
-    );
+    version = genDateVersion inputs.coq_artifacts;
     src = inputs.coq_artifacts;
     meta.homepage = inputs.coq_artifacts.url;
   };
 
   coq_thirdparty = vimUtils.buildVimPluginFrom2Nix {
     pname = "coq_thirdparty";
-    version = with lib; builtins.head (
-      splitString " " (
-        last (
-          splitString
-            "\n"
-            (fileContents "${inputs.coq_thirdparty}/.github/.agp")
-        )
-      )
-    );
+    version = genDateVersion inputs.coq_thirdparty;
     src = inputs.coq_thirdparty;
     meta.homepage = inputs.coq_thirdparty.url;
   };
 
   due_nvim = vimUtils.buildVimPluginFrom2Nix {
     pname = "due_nvim";
-    version = "2021-07-04";
+    version = genDateVersion inputs.due_nvim;
     src = inputs.due_nvim;
     meta.homepage = inputs.due_nvim.url;
   };
 
   nabla-nvim = vimUtils.buildVimPluginFrom2Nix {
     pname = "nabla.nvim";
-    version = "2021-10-20";
+    version = genDateVersion inputs.nabla-nvim;
     src = inputs.nabla-nvim;
     meta.homepage = inputs.nabla-nvim.url;
   };
 
   neorg-telescope = vimUtils.buildVimPluginFrom2Nix {
     pname = "neorg-telescope";
-    version = "2021-08-18";
+    version = genDateVersion inputs.neorg-telescope;
     src = inputs.neorg-telescope;
     meta.homepage = inputs.neorg-telescope.url;
   };
 
   nvim-workbench = vimUtils.buildVimPluginFrom2Nix {
     pname = "nvim-workbench";
-    version = "2021-07-12";
+    version = genDateVersion inputs.nvim-workbench;
     src = inputs.nvim-workbench;
     meta.homepage = inputs.nvim-workbench.url;
   };
 
   vim-latex = {
     pname = "vim-latex";
-    version = "2021-08-18";
+    version = genDateVersion inputs.vim-latex;
     src = inputs.vim-latex;
     meta.homepage = inputs.vim-latex.url;
   };
 
   windline-nvim = {
     pname = "windline.nvim";
-    version = "2021-10-21";
+    version = genDateVersion inputs.windline-nvim;
     src = inputs.windline-nvim;
     meta.homepage = inputs.windline-nvim.url;
   };

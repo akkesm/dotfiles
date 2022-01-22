392e62abd1de815a637457fc44b1ef200e32600b
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-01-22 11:21:18 +0100

    lockfile: update

--- home/configuration/gtk.nix
+++ home/configuration/gtk.nix
@@ -5,6 +5,16 @@
     enable = true;
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
 
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+      gtk-cursor-theme-name = "Nordzy-cursors";
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+      gtk-cursor-theme-name = "Nordzy-cursors";
+    };
+
     iconTheme = {
       package = pkgs.nordzy-icon-theme;
       name = "Nordzy-orange-dark";
@@ -16,7 +26,10 @@
     };
   };
 
-  home.packages = [ pkgs.hicolor-icon-theme ];
+  home.packages = with pkgs; [
+    hicolor-icon-theme
+    nordzy-cursors
+  ];
 
   wayland.windowManager.sway.wrapperFeatures.gtk = config.wayland.windowManager.sway.enable;
 }

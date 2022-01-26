b6f787697e6ff5b232a321d652ca1d6c7f905c03
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-01-26 17:03:48 +0100

    home: edit beets configuration

--- home/configuration/media/default.nix
+++ home/configuration/media/default.nix
@@ -4,6 +4,54 @@
   imports = [ ./easyeffects.nix ];
 
   programs = {
+    beets = {
+      enable = true;
+
+      package = pkgs.beets.override {
+        enableAlternatives = true;
+        enableExtraFiles = true;
+      };
+
+      settings = {
+        library = "${config.xdg.dataHome}/beets/library.db";
+        directory = config.xdg.userDirs.music;
+        plugins = [
+          "deezer"
+          "edit"
+          "fuzzy"
+          "missing"
+          "mpdupdate"
+          "spotify"
+        ];
+
+        ignore = [
+          ".*"
+          "*~"
+          "System Volume Information"
+          "lost+found"
+          "*.m3u"
+          "*.txt"
+        ];
+
+        format_album = "$year - $albumartist - $album";
+        sort_album = "albumartist+ year+";
+
+        import = {
+          reflink = "auto";
+          incremental_skip_later = "yes";
+        };
+
+        musicbrainz.extratags = [ "year" ];
+
+        match = {
+          preferred.original_year = "yes";
+          ignored = [ "track_length" ];
+        };
+
+        paths.default = "$albumartist/$year-$album%aunique{}/$track-$title";
+      };
+    };
+
     mpv = {
       enable = true;
 

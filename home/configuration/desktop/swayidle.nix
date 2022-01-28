7b709b2a67687ec7b4bc0b88bafc8443ecade877
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-01-28 19:51:35 +0100

    home: use the swayidle option

--- home/configuration/desktop/swayidle.nix
+++ home/configuration/desktop/swayidle.nix
@@ -1,27 +1,47 @@
 { config, pkgs, ... }:
 
 {
-  systemd.user.services.swayidle = {
-    Unit = {
-      Description = "Idle Manager for Wayland";
-      Documentation = [ "man:swayidle(1)" ];
-      PartOf = [ "graphical-session.target" ];
-    };
+  services.swayidle = {
+    enable = true;
 
-    Install = { WantedBy = [ "sway-session.target" ]; };
+    events = [
+      {
+        event = "lock";
+        command = "${pkgs.swaylock}/bin/swaylock -f";
+      }
+      {
+        event = "before-sleep";
+        command = "${pkgs.playerctl}/bin/playerctl pause";
+      }
+      {
+        event = "before-sleep";
+        command = "${pkgs.swaylock}/bin/swaylock -f";
+      }
+      {
+        event = "after-resume";
+        command = "${pkgs.sway}/bin/swaymsg 'output * dpms on'";
+      }
+      {
+        event = "after-resume";
+        command = "${pkgs.brightnessctl}/bin/brightnessctl set 6%";
+      }
+    ];
 
-    Service = {
-      Type = "simple";
-      ExecStart = ''
-        ${pkgs.swayidle}/bin/swayidle -w \
-          timeout 480 "${pkgs.brightnessctl}/bin/brightnessctl set 0%" \
-          timeout 600 "${pkgs.sway}/bin/swaymsg 'output * dpms off'" \
-          timeout 605 "${pkgs.swaylock}/bin/swaylock" \
-          resume "${pkgs.sway}/bin/swaymsg 'output * dpms on'" \
-          resume "${pkgs.brightnessctl}/bin/brightnessctl set 6%" \
-          before-sleep "${pkgs.playerctl}/bin/playerctl pause" \
-          before-sleep "${pkgs.swaylock}/bin/swaylock"
-      '';
-    };
+    timeouts = [
+      {
+        timeout = 480;
+        command = "${pkgs.brightnessctl}/bin/brightnessctl set 0%";
+        resumeCommand = "${pkgs.brightnessctl}/bin/brightnessctl set 6%";
+      }
+      {
+        timeout = 600;
+        command = "${pkgs.sway}/bin/swaymsg 'output * dpms off'";
+        resumeCommand = "${pkgs.sway}/bin/swaymsg 'output * dpms on' && ${pkgs.brightnessctl}/bin/brightnessctl set 6%";
+      }
+      {
+        timeout = 605;
+        command = "${pkgs.swaylock}/bin/swaylock -f";
+      }
+    ];
   };
 }

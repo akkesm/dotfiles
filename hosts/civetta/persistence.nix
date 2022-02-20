565f315bc538b6b50eb204b2d89e84948b7224d2
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-02-20 14:15:48 +0100

    civetta: link dotfiles to /etc/nixos

--- hosts/civetta/persistence.nix
+++ hosts/civetta/persistence.nix
@@ -2,15 +2,19 @@
 
 {
   environment = {
-    persistence."/persist".files = [
-      "/etc/machine-id"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
+    persistence."/persist" = {
+      directories = [ "/etc/nixos" ];
 
-      "/var/yubico/alessandro-15454659"
-    ];
+      files = [
+        "/etc/machine-id"
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
+        "/etc/ssh/ssh_host_rsa_key"
+        "/etc/ssh/ssh_host_rsa_key.pub"
+
+        "/var/yubico/alessandro-15454659"
+      ];
+    };
 
     systemPackages = [ pkgs.fs-diff ];
   };

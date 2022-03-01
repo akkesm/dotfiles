b1b12e118859c697b52b58eb674db2a431269a80
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-03-01 13:10:27 +0100

    home: zathura: apply nord theme

--- home/configuration/applications/documents.nix
+++ home/configuration/applications/documents.nix
@@ -22,6 +22,44 @@
         statusbar-home-tilde = true;
         zoom-max = 500;
         selection-notification = false;
+
+        # https://github.com/nautilor/zathura-nord
+        notification-error-bg = "#2E3440";
+        notification-error-fg = "#BF616A";
+        notification-warning-bg = "#2E3440";
+        notification-warning-fg = "#D08770";
+        notification-bg = "#2E3440";
+        notification-fg = "#D8DEE9";
+        
+        completion-bg = "#2E3440";
+        completion-fg = "#D8DEE9";
+        completion-group-bg = "#3B4252";
+        completion-group-fg = "#D8DEE9";
+        completion-highlight-bg = "#88C0D0";
+        completion-highlight-fg = "#3B4252";
+        
+        index-bg = "#2E3440";
+        index-fg = "#8FBCBB";
+        index-active-bg = "#8FBCBB";
+        index-active-fg = "#2E3440";
+        
+        inputbar-bg = "#2E3440";
+        inputbar-fg = "#E5E9F0";
+        
+        statusbar-bg = "#2E3440";
+        statusbar-fg = "#E5E9F0";
+        
+        highlight-color = "#D08770";
+        highlight-active-color = "#BF616A";
+        
+        default-bg = "#2E3440";
+        default-fg = "#D8DEE9";
+        render-loading = "true";
+        render-loading-bg = "#2E3440";
+        render-loading-fg = "#434C5E";
+        
+        recolor-lightcolor = "#2E3440";
+        recolor-darkcolor = "#ECEFF4";
       };
     };
   };

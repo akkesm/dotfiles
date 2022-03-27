63dad3b45e8572bbd168cb9d2760493eb792fa0f
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-03-27 12:05:21 +0200

    civetta: kvm: add options for OSX-KVM

--- hosts/civetta/virtualisation.nix
+++ hosts/civetta/virtualisation.nix
@@ -1,7 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  boot.initrd.kernelModules = [ "kvm-amd" ];
+  boot = {
+    # https://nixos.wiki/wiki/OSX-KVM
+    extraModprobeConfig = ''
+      options kvm_amd nested=1
+      options kvm_amd emulate_invalid_guest_state=0
+      options kvm ignore_msrs=1
+    '';
+
+    initrd.kernelModules = [ "kvm-amd" ];
+  };
   
   # http://bkanuka.com/posts/windows-10-libvirt/
   virtualisation.libvirtd = { # Add users to "libvirtd" group

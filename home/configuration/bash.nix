2395128e6e7fd5fdf51ca401cf18ba3b36ba37cd
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-03-27 12:01:58 +0200

    home: bash: set erasedups

--- home/configuration/bash.nix
+++ home/configuration/bash.nix
@@ -3,6 +3,12 @@
 {
   programs.bash = {
     enable = true;
+
+    historyControl = [
+      "erasedups"
+      "ignorespace"
+    ];
+
     historyFileSize = 1000;
     profileExtra = ''
       if [ -e ${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh ]; then

74862ddf7ba3fe9961fa232f0ef153ea52a55177
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-04-07 13:08:52 +0200

    home: neovim: fix chadtree -> tree

--- home/configuration/neovim/default.nix
+++ home/configuration/neovim/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./chadtree.nix
     ./colorscheme.nix
     # ./compleet.nix
     ./coq.nix
@@ -76,29 +77,6 @@
         '';
       }
       vim-asterisk
-
-      # file explorer
-      {
-        plugin = chadtree;
-        config = ''
-          let g:chadtree_settings = {
-          \ 'xdg': v:true,
-          \ 'ignore.name_exact': [],
-          \ 'view': {
-          \   'sort_by': [ 'is_folder', 'file_name', 'ext' ],
-          \   'width': 30
-          \ },
-          \ 'theme.text_colour_set' : 'nord'
-          \}
-
-          nnoremap <Leader>t <Cmd>CHADopen<CR>
-
-          " Start CHADTree when Vim starts with a directory argument
-          autocmd StdinReadPre * let s:std_in=1
-          autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') |
-          \ execute 'CHADopen' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0] | endif
-        '';
-      }
 
       # lf integration
       {

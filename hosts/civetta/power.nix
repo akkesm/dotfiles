7d97f66d4e32a23eb7dca5030e9957cc46f4468e
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-05-12 09:41:35 +0200

    civetta: change various TLP settings

--- hosts/civetta/power.nix
+++ hosts/civetta/power.nix
@@ -6,20 +6,21 @@
       enable = true;
       settings = {
         SOUND_POWER_SAVE_ON_AC = 0;
-        SOUND_POWER_SAVE_ON_BAT = 0;
+        SOUND_POWER_SAVE_ON_BAT = 10;
         SOUND_POWER_SAVE_CONTROLLER = "Y";
         START_CHARGE_THRESH_BAT0 = 60;
         STOP_CHARGE_THRESH_BAT0 = 85;
-        NATACPI_ENABLE = 1;
-        TPACPI_ENABLE = 0;
-        TPSMAPI_ENABLE = 0;
+        DISK_IOSCHED = "keep keep";
+        WIFI_PWR_ON_BAT = "off";
+        WOL_DISABLE = "N";
         CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
         CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
         CPU_SCALING_MIN_FREQ_ON_AC = 1400000;
         CPU_SCALING_MAX_FREQ_ON_AC = 2900000;
         CPU_SCALING_MIN_FREQ_ON_BAT = 1400000;
         CPU_SCALING_MAX_FREQ_ON_BAT = 2900000;
-        RESTORE_DEVICE_STATE_ON_STARTUP = 1;
+        DEVICES_TO_DISABLE_ON_STARTUP = "wwan";
+        DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth";
         USB_AUTOSUSPEND = 0;
       };
     };

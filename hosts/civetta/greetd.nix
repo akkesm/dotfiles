a7168de4a1b838e258b1c97f1df6f80d25ee016e
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-05-12 13:13:42 +0200

    civetta: fix greetd non-autologin sessions

--- hosts/civetta/greetd.nix
+++ hosts/civetta/greetd.nix
@@ -1,25 +1,12 @@
 { config, pkgs, ... }:
 
-let
-  wlgreetSwayConfig = pkgs.writeText "wlgreet-sway-config" ''
-    exec "${pkgs.greetd.wlgreet}/bin/wlgreet --command ${pkgs.sway}/bin/sway; ${pkgs.sway}/bin/swaymsg exit"
-
-    bindsym Mod4+Shift+e exec swaynag \
-    	-t warning \
-    	-m 'What do you want to do?' \
-    	-b 'Poweroff' 'systemctl poweroff' \
-    	-b 'Reboot' 'systemctl reboot'
-  '';
-in
 {
-  environment.systemPackages = [ pkgs.wayland ];
-
   services.greetd = {
     enable = true;
     package = pkgs.greetd.greetd;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.greetd}/bin/agreety --cmd $SHELL";
+        command = "${pkgs.greetd.greetd}/bin/agreety --cmd ${pkgs.zsh}/bin/zsh";
         user = "greeter";
       };
 

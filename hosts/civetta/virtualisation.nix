6f1a2af99d129d27d869485db9b7215d082d3009
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-06-29 11:12:38 +0200

    civetta: add windows vm docs

--- hosts/civetta/virtualisation.nix
+++ hosts/civetta/virtualisation.nix
@@ -13,6 +13,7 @@
   };
 
   # http://bkanuka.com/posts/windows-10-libvirt/
+  # https://kevinlocke.name/bits/2021/12/10/windows-11-guest-virtio-libvirt/
   virtualisation.libvirtd = {
     # Add users to "libvirtd" group
     enable = true;

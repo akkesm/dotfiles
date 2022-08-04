54897708b9aca965dc7339f6c893f3891089701c
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-08-04 14:15:16 +0200

    live: remove deprecated option xdg.gtkUsePortal

--- hosts/live/default.nix
+++ hosts/live/default.nix
@@ -26,13 +26,12 @@
 
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
 
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-kde
     ];
 
-    gtkUsePortal = true;
+    wlr.enable = true;
   };
 }

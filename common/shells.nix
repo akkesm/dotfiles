4ba68a06fe3bbecb2c93fa8cb7fdcb34f3a6bd8d
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-08-30 17:49:57 +0200

    common: remove any-nix-shell, delete unused aliases

--- common/shells.nix
+++ common/shells.nix
@@ -9,23 +9,10 @@
 
       cdtmp = "cd $(mktemp -d)";
 
-      dnfl = "dnf list";
-      dnfli = "dnf list installed";
-      dnfq = "dnf info";
-      dnfs = "dnf search";
-      dnfdep = "dnf repoquery --requires --resolve";
-      dnfreq = "dnf repoquery --exactdeps --whatrequires";
-      dnfp = "dnf provides";
-      dnfa = "sudo dnf autoremove";
-      dnfc = "sudo dnf clean all";
-      dnfi = "sudo dnf install";
-      dnfr = "sudo dnf remove";
-      dnfu = "sudo dnf upgrade";
+      ll = "${pkgs.exa}/bin/exa --long --group-directories-first --links --binary --group --time-style long-iso --icons";
+      la = "${pkgs.exa}/bin/exa --long --group-directories-first --links --binary --group --time-style long-iso --icons --all";
 
-      ll = "exa --long --group-directories-first --links --binary --group --time-style long-iso --icons";
-      la = "exa --long --group-directories-first --links --binary --group --time-style long-iso --icons --all";
-
-      gits = "git status";
+      gits = "${pkgs.git}/bin/git status";
 
       h = "history";
       hs = "history | ${pkgs.ripgrep}/bin/rg -i";
@@ -35,16 +22,12 @@
       pkgs.bashInteractive
       pkgs.zsh
     ];
-
-    systemPackages = [ pkgs.any-nix-shell ];
   };
 
   programs.zsh = {
     enableBashCompletion = true;
     enableCompletion = true;
-
-    promptInit = ''
-      any-nix-shell zsh | source /dev/stdin
-    '';
   };
+
+  users.defaultUserShell = pkgs.zsh;
 }

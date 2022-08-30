9efc19e2f72c21fb08528d22b21a23f62be03ac4
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-08-30 17:51:11 +0200

    civetta: remove unnecessary options or move them to common

--- hosts/civetta/users.nix
+++ hosts/civetta/users.nix
@@ -1,48 +1,29 @@
 { config, pkgs, ... }:
 
 {
-  users = {
-    defaultUserShell = pkgs.zsh;
-    mutableUsers = false;
+  users.users.alessandro = {
+    description = "Alessandro Barenghi";
 
-    users."alessandro" = {
-      description = "Alessandro Barenghi";
+    extraGroups = [
+      "adbusers"
+      "keys"
+      "libvirtd"
+      "lp"
+      "podman"
+      "scanner"
+      "video"
+      "wheel"
+    ];
 
-      extraGroups = [
-        "adbusers"
-        "keys"
-        "libvirtd"
-        "lp"
-        "podman"
-        "scanner"
-        "video"
-        "wheel"
-      ];
-
-      initialHashedPassword = "$6$Hx7jqQUP$WEm9LBVxX/BJGZI3e1NqlYtHTyMERYLEl02Q5e3dGFLPvVzYpvr9ULXcxNF4K1SLWRHnnscZv5qBTpGxAsPSy1";
-      isNormalUser = true;
-      passwordFile = config.sops.secrets.password-alessandro.path;
-      name = "alessandro";
-      uid = 1000;
-    };
+    isNormalUser = true;
+    passwordFile = config.sops.secrets.password-alessandro.path;
+    uid = 1000;
   };
 
-  security = {
-    sudo = {
-      execWheelOnly = true;
-
-      extraConfig = ''
-        Defaults lecture = never
-      '';
-
-      wheelNeedsPassword = false;
-    };
-
-    pam.yubico = {
-      enable = true;
-      challengeResponsePath = "/var/yubico";
-      mode = "challenge-response";
-    };
+  security.pam.yubico = {
+    enable = true;
+    challengeResponsePath = "/var/yubico";
+    mode = "challenge-response";
   };
 
   sops.secrets = {
@@ -60,49 +41,4 @@
       sopsFile = ./secrets/cachix.yaml;
     };
   };
-
-  # TODO make into module 
-  # environment = {
-  #   systemPackages = [ pkgs.ly ];
-  #   etc."ly/config.ini".text = ''
-  #     animate = false
-  #     animation = 0
-  #     asterisk = *
-  #     bg = 0
-  #     blank_password = false
-  #     fg = 9
-  #     hide_borders = true
-  #     lang = it
-  #     mcookie_cmd = ${pkgs.util-linux}/bin/mcookie
-  #     path = /bin:/usr/bin
-  #     restart_cmd = ${pkgs.systemd}/bin/systemctl reboot
-  #     save_file = /persist/etc/ly/save
-  #     shutdown = ${pkgs.systemd}/bin/systemctl poweroff
-  #     term_reset_cmd = ${pkgs.ncurses}/bin/tput rest
-  #   '';
-  # };
-  # security.pam.services."ly".text = ''
-  #   #%PAM-1.0
-
-  #   auth       include      login
-  #   account    include      login
-  #   password   include      login
-  #   session    include      login
-  # '';
-  # systemd.services."ly" = {
-  #   aliases = [ "display-manager.service" ];
-  #   after = [
-  #     "systemd-user-sessions.service plymouth-quit-wait.service"
-  #     "getty@tty2.service"
-  #   ];
-  #   description = "TUI display manager";
-  #   serviceConfig = {
-  #     oType = "idle";
-  #     ExecStart = "/usr/bin/ly";
-  #     StandardInput = "tty";
-  #     TTYPath = "/dev/tty2";
-  #     TTYReset = "yes";
-  #     TTYVHangup = "yes";
-  #   };
-  # };
 }

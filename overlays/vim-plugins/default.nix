ed937523fa9d1b1e5f6c523dcb354c5c58b3c3fd
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-10-08 10:26:13 +0200

    overlays: fix vim plugins

--- overlays/vim-plugins/default.nix
+++ overlays/vim-plugins/default.nix
@@ -8,27 +8,23 @@ in
     pname = "dirbuf.nvim";
     version = genDateVersion inputs.dirbuf-nvim;
     src = inputs.dirbuf-nvim;
-    meta.homepage = inputs.dirbuf-nvim.url;
   };
 
   due_nvim = vimUtils.buildVimPluginFrom2Nix {
     pname = "due_nvim";
     version = genDateVersion inputs.due_nvim;
     src = inputs.due_nvim;
-    meta.homepage = inputs.due_nvim.url;
   };
 
   neorg-telescope = vimUtils.buildVimPluginFrom2Nix {
     pname = "neorg-telescope";
     version = genDateVersion inputs.neorg-telescope;
     src = inputs.neorg-telescope;
-    meta.homepage = inputs.neorg-telescope.url;
   };
 
   vim-latex = {
     pname = "vim-latex";
     version = genDateVersion inputs.vim-latex;
     src = inputs.vim-latex;
-    meta.homepage = inputs.vim-latex.url;
   };
 }

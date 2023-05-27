8905794dddbe1410c0398cc1cc279fce5347a05b
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2023-05-27 11:20:30 +0200

    common: disable unused caches

--- common/nix.nix
+++ common/nix.nix
@@ -23,8 +23,8 @@
         "https://nix-community.cachix.org"
         # "https://nixpkgs-wayland.cachix.org"
         "https://akkesm.cachix.org"
-        "https://digitallyinduced.cachix.org"
-        "https://cache.iog.io"
+        # "https://digitallyinduced.cachix.org"
+       # "https://cache.iog.io"
         # "https://nickel.cachix.org"
         # "https://nixcache.reflex-frp.org"
       ];
@@ -35,8 +35,8 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         # "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "akkesm.cachix.org-1:9ESYmocqudg/QaRhELpkSWy4LNZbTqDdD2N0zpJP/e8="
-        "digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE="
-        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        # "digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE="
+      # "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         # "nickel.cachix.org-1:ABoCOGpTJbAum7U6c+04VbjvLxG9f0gJP5kYihRRdQs="
         # "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
       ];

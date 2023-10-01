a78ec4a88f24086a8ccef48503307b207ca64e0a
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2023-10-01 17:44:39 +0200

    civetta: add config for Vagrant

--- hosts/civetta/virtualisation.nix
+++ hosts/civetta/virtualisation.nix
@@ -21,6 +21,19 @@
   };
 
   environment.systemPackages = [ pkgs.virt-manager ];
+
+  # Vagrant
+  services.nfs.server.enable = true;
+
+  networking.firewall.extraCommands = ''
+    ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
+  '';
+
+  # Add firewall exception for libvirt provider when using NFSv4
+  networking.firewall.interfaces."virbr1" = {
+    allowedTCPPorts = [ 2049 ];
+    allowedUDPPorts = [ 2049 ];
+  };
 }
 
 # Windows VM optimization options

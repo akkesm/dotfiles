e4db750f4994d578ea9a02c7cd65cfec8ba2ea9f
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2024-03-10 13:21:02 +0100

    home: sway: new window rule for copyq

--- home/configuration/desktop/clipboard.nix
+++ home/configuration/desktop/clipboard.nix
@@ -4,7 +4,11 @@
   home.packages = [ pkgs.wl-clipboard ];
   services.copyq.enable = true;
 
-  wayland.windowManager.sway.config.keybindings =
-    let mod = config.wayland.windowManager.sway.config.modifier;
-    in lib.mkOptionDefault { "${mod}+p" = "exec ${pkgs.copyq}/bin/copyq show"; };
+  wayland.windowManager.sway.config = {
+    floating.criteria = [{ class = "copyq"; }];
+
+    keybindings =
+      let mod = config.wayland.windowManager.sway.config.modifier;
+      in lib.mkOptionDefault { "${mod}+p" = "exec ${pkgs.copyq}/bin/copyq show"; };
+  };
 }

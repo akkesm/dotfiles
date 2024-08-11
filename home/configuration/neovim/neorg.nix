d3abcd78644058f54fec0546e7d97282717d2ec3
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2024-08-11 17:13:35 +0200

    home: neovim: added missing dependencies for neorg

--- home/configuration/neovim/neorg.nix
+++ home/configuration/neovim/neorg.nix
@@ -1,38 +1,46 @@
 { config, pkgs, ... }:
 
 {
-  programs.neovim.plugins = with pkgs.vimPlugins; [
-    {
-      plugin = neorg;
-      type = "lua";
-      config = ''
-        require('neorg').setup {
-          load = {
-            ['core.defaults'] = {},
+  programs.neovim = {
+    extraLuaPackages = (ps: with ps; [
+      lua-utils-nvim
+      nvim-nio
+      pathlib-nvim
+    ]);
 
-            ['core.concealer'] = { config = { folds = false } },
-            ['core.esupports.metagen'] = { config = { type = 'empty' } },
-            ['core.export'] = {},
-            ['core.export.markdown'] = { config = { extensions = 'all' } },
-            ['core.integrations.treesitter'] = {},
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = neorg;
+        type = "lua";
+        config = ''
+          require('neorg').setup {
+            load = {
+              ['core.defaults'] = {},
 
-            ['core.dirman'] = {
-              config = {
-                autodetect = true,
-                autochdir = true,
-                ['workspaces.uni'] = "${config.xdg.userDirs.documents}/UNI/notes",
+              ['core.concealer'] = { config = { folds = false } },
+              ['core.esupports.metagen'] = { config = { type = 'empty' } },
+              ['core.export'] = {},
+              ['core.export.markdown'] = { config = { extensions = 'all' } },
+              ['core.integrations.treesitter'] = {},
+
+              ['core.dirman'] = {
+                config = {
+                  autodetect = true,
+                  autochdir = true,
+                  ['workspaces.uni'] = "${config.xdg.userDirs.documents}/UNI/notes",
+                },
               },
+
+              -- External modules
+              ['core.integrations.telescope'] = {},
             },
 
-            -- External modules
-            ['core.integrations.telescope'] = {},
-          },
+            ['logger.use_file'] = true,
+          }
+        '';
+      }
 
-          ['logger.use_file'] = true,
-        }
-      '';
-    }
-
-    neorg-telescope
-  ];
+      neorg-telescope
+    ];
+  };
 }

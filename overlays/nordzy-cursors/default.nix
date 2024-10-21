1c3455024bc397dcd25ef72851727ed5332a7172
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2024-10-21 22:04:40 +0200

    overlays: nordzy-cursors: 1.0.0 -> 2.3.0

--- overlays/nordzy-cursors/default.nix
+++ overlays/nordzy-cursors/default.nix
@@ -2,26 +2,27 @@
 
 stdenv.mkDerivation rec {
   pname = "nordzy-cursors";
-  version = "1.0.0";
+  version = "2.3.0";
 
   src = fetchFromGitHub {
     owner = "alvatip";
     repo = "Nordzy-cursors";
-    rev = version;
-    sha256 = "12mpwmaf4jjbg09nzxb6dpj9pfbb1iq61c484f4cqf6jdkl9znim";
+    rev = "v${version}";
+    sha256 = "15ahcc9glv58cn6pa9sf0fp6yiiq4sbxyknsaywaqwch82bi4xfw";
   };
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out/share/icons
-    cp -r themes/* $out/share/icons
+    cp -r xcursors/* $out/share/icons
+    cp -r hyprcursors/themes/* $out/share/icons
 
     runHook postInstall
   '';
 
   meta = with lib; {
-    description = "Cursor theme using the Nord color palette";
+    description = "Cursor theme using the Nord color palette and based on Vimix and cz-Viator";
     homepage = "https://github.com/alvatip/Nordzy-cursors";
     license = licenses.gpl3Only;
     platforms = platforms.unix;

27d2a58dffb71657999efd829da6bccebae1c48d
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2024-11-17 13:46:32 +0100

    civetta: update deprecated package names

--- hosts/civetta/amdgpu.nix
+++ hosts/civetta/amdgpu.nix
@@ -12,8 +12,8 @@
     enable32Bit = true;
 
     extraPackages = with pkgs; [
-      rocm-opencl-icd
-      rocm-opencl-runtime
+      rocmPackages.clr.icd
+      rocmPackages.clr
       amdvlk
     ];
 

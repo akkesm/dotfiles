7428443624f78d313e8a46f0c925521f705d91b2
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2025-12-14 13:11:54 +0100

    common: add localisation details

--- common/localisation.nix
+++ common/localisation.nix
@@ -5,9 +5,14 @@
 
   i18n = {
     defaultLocale = "it_IT.UTF-8";
-    extraLocaleSettings.LC_MESSAGES = "en_US.UTF-8";
 
-    supportedLocales = [
+    extraLocaleSettings = {
+      LC_MESSAGES = "en_US.UTF-8";
+      LC_NUMERIC = "C.UTF-8";
+      LC_TIME = "it_IT.UTF-8";
+    };
+
+    extraLocales = [
       "en_GB.UTF-8/UTF-8"
       "en_US.UTF-8/UTF-8"
       "es_ES.UTF-8/UTF-8"
@@ -19,3 +24,4 @@
 
   time.timeZone = "Europe/Rome";
 }
+

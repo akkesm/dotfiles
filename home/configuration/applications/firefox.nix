1dd12c62e289a0828c6e3ec56254354fc42dffc1
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2026-09-03 20:34:52 +0200

    home: firefox: disable FPI

--- home/configuration/applications/firefox.nix
+++ home/configuration/applications/firefox.nix
@@ -128,7 +128,7 @@
         "privacy.trackingprotection.enabled" = true;
         "privacy.trackingprotection.socialtracking.enabled" = true;
         # "toolkit.telemetry.enabled" = false; # when true send anonymous reports to Mozilla
-        "privacy.firstparty.isolate" = true; # Can break third-party logins
+        "privacy.firstparty.isolate" = false; # Can break third-party logins
         "privacy.resistFingerprinting" = false; #!! RFP !! This is the setting
         "browser.send_pings" = false; # default
         "network.http.referer.XOriginPolicy" = 1; # A value of 0 can break stuff
